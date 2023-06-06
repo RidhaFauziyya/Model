64ed2b449d9c0672c4fa255e1e3a91c52837a84c
RidhaFauziyya <ridhafauziyyar123@gmail.com>
2023-06-06 23:49:35 +0700

    mvvm

---
+++ app/src/main/java/com/example/myviewmodel/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.myviewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel viewModel;
+    private EditText edtLength, edtWidth, edtHeight;
+    private Button btnCalculate;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        edtHeight = findViewById(R.id.edt_height);
+        edtWidth = findViewById(R.id.edt_width);
+        edtLength = findViewById(R.id.edt_length);
+
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate = findViewById(R.id.btn_calculate);
+        btnCalculate.setOnClickListener(view -> {
+            String length = edtLength.getText().toString();
+            String width = edtWidth.getText().toString();
+            String height = edtHeight.getText().toString();
+
+            if(width.isEmpty()){
+                edtWidth.setError("Please enter width!");
+            }else if (length.isEmpty()){
+                edtLength.setError("Please enter length!");
+            }else if (height.isEmpty()){
+                edtHeight.setError("Please enter height!");
+            }else{
+                
+//                String result = String.valueOf(Integer.parseInt(width) * Integer.parseInt(height) * Integer.parseInt(length));
+//                tvResult.setText(result);
+                viewModel.calculate(width, height, length);
+            }
+        });
+        
+        displayResult();
+
+    }
+
+    private void displayResult(){
+        viewModel.result.observe(this, new Observer<Integer>(){
+            @Override
+            public void onChanged(Integer result){
+                tvResult.setText(String.valueOf(result));
+            }
+        });
+    }
+
+}
